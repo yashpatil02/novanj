992dc6ff3a623198c8d1ed97f9d4c6799b6c1e7c
yashpatil02 <yashpatil121199@gmail.com>
2024-03-21 14:26:08 +0530

    new

--- src/components/NavBars.jsx
+++ src/components/NavBars.jsx
@@ -19,16 +19,24 @@ const CustomNavbar = () => {
   };
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
       <Container>
-    <Navbar.Brand as={Link} to="/">Your Logo</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/">Novanj</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="ml-auto ms-auto"  variant="underline" >
+      <Nav className="ml-auto ms-auto" variant='underline'  >
+        <Nav.Item className='me-4'>
         <Nav.Link eventKey="link-1" as={Link} to="/" onClick={() => scrollToSection('home')}>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='me-4'>
         <Nav.Link eventKey="link-2" as={Link} to="/about">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='me-4'>
         <Nav.Link eventKey="link-3" as={Link} to="/services">Services</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='me-4'>
         <Nav.Link eventKey="link-4" onClick={() => scrollToSection('contact')}>Contact</Nav.Link>
+        </Nav.Item>
       </Nav>
     </Navbar.Collapse>
     </Container>  
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,76 +1,56 @@
 // Home.js
 import React from 'react';
+import { useState } from 'react';
 import CustomNavbar from "../NavBars";
+import Carousel from 'react-bootstrap/Carousel';
+
+
 const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
     <div>
      <div> 
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
-        <p>yash</p>
+     <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400?text=First+Slide"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400?text=Second+Slide"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400?text=Third+Slide"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>  
      </div>
     </div>
     
