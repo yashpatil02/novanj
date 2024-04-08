ffe1a77fdc8130804b2c64dfefd5aa1b5da4c6f7
yashpatil02 <yashpatil121199@gmail.com>
2024-04-08 10:40:59 +0530

    updata website

--- src/components/NavBars.jsx
+++ src/components/NavBars.jsx
@@ -1,5 +1,5 @@
 // Navbar.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { Link, useLocation } from 'react-router-dom';
@@ -7,6 +7,7 @@ import './NavBar.css';
 
 const CustomNavbar = () => {
   const location = useLocation();
+  const [navbarBg, setNavbarBg] = useState('rgba(0, 0, 0, 0.627)');
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -18,29 +19,48 @@ const CustomNavbar = () => {
     }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 200) {
+        setNavbarBg('light');
+        document.querySelector('.navbar').classList.add('scrolled');
+      } else {
+        setNavbarBg('rgba(0, 0, 0, 0.627)');
+        document.querySelector('.navbar').classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+    <Navbar collapseOnSelect expand="lg" bg={navbarBg} variant="light" sticky="top" className="fixed-top">
       <Container>
-    <Navbar.Brand as={Link} to="/">Novanj</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="ml-auto ms-auto" variant='underline'  >
-        <Nav.Item className='me-4'>
-        <Nav.Link eventKey="link-1" as={Link} to="/" onClick={() => scrollToSection('home')}>Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='me-4'>
-        <Nav.Link eventKey="link-2" as={Link} to="/about">About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='me-4'>
-        <Nav.Link eventKey="link-3" as={Link} to="/services">Services</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='me-4'>
-        <Nav.Link eventKey="link-4" onClick={() => scrollToSection('contact')}>Contact</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Navbar.Collapse>
-    </Container>  
-  </Navbar>
+        <Navbar.Brand as={Link} to="/"><img src="assets/images/logo.png" alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto ms-auto" variant='underline'>
+            <Nav.Item className='me-4'>
+              <Nav.Link eventKey="link-1" as={Link} to="/" onClick={() => scrollToSection('home')}>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='me-4'>
+              <Nav.Link eventKey="link-2" as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='me-4'>
+              <Nav.Link eventKey="link-3" as={Link} to="/services">Services</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className='me-4'>
+              <Nav.Link eventKey="link-4" onClick={() => scrollToSection('contact')}>Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
-// Home.js
 import React from 'react';
 import { useState } from 'react';
 import CustomNavbar from "../NavBars";
 import Carousel from 'react-bootstrap/Carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
+import './Home.css';
 
 const Home = () => {
   const [index, setIndex] = useState(0);
@@ -11,49 +13,25 @@ const Home = () => {
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
+
   return (
     <div>
-     <div> 
-     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=First+Slide"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=Second+Slide"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing yash.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=Third+Slide"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>  
-     </div>
+    <div className="bg-image-container">
+      <div className="heading1">
+        <span className='sport'>Sports</span> 
+        <span className='analysis'>Analysis</span>
+      </div>
+      <img className="bg-image img-fluid" src="assets/images/pngwing.png" alt="" />
+      <div className="overlay">
+        <div className="text-white">
+          <p className='text-right'>Lorem ipsum dolor, sit amet consectetur  elit. <br /> nulla magni sunt quos numquam unde fugit?</p>
+        </div>
+        <a className='scroll-down'>Scroll down <FontAwesomeIcon icon={faAngleDown} /></a>
+
+      </div>
     </div>
-    
+
+    </div>
   );
 }
 
