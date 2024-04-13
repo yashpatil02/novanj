9d7c99ba7d6864421e7c6e0b0de617bce5dc3be2
shilesh-mavchi <mavchi1212@gmail.com>
2024-04-13 18:03:14 +0530

    footer and Services done

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -6,66 +6,84 @@ function Footer() {
     // Array of image paths
     const imagePaths = [
         '/assets/images/1.png',
-        '/assets/images/ballb.png',
+        '/assets/images/1.png',
+        '/assets/images/1.png',
+        '/assets/images/1.png',
+        '/assets/images/1.png',
+        '/assets/images/1.png',
+
+
     ];
+
     return (
         <div className="footer">
             <Container>
                 <footer>
                     <Row className="footer-content">
-                        <Col md={3} className="footer-section">
-                            <h5 className="footer-heading">Product</h5>
-                            <ul className="footer-list">
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                            </ul>
+                        <Col md={8}>
+                            <Row >
+                                <Col md={6} className="footer-section">
+                                    <h5 className="footer-heading">Novanj Company</h5>
+                                    <span>Novanj: Leading sports analysis provider empowering athletes, coaches, and teams worldwide with innovative tools and data-driven strategies for success.</span>
+                                </Col>
+                                <Col md={6} className="footer-section">
+                                    <h5 className="footer-heading">Contact Us</h5>
+                                    <div className="contact-info">
+                                        <p >
+                                            <i className="fas fa-phone"></i> Phone: +123456789<br />
+                                            <i className="fas fa-envelope"></i> Email: info@novanj.com<br />
+                                            <i className="fas fa-map-marker-alt"></i> Novanj Sports Analysis<br />
+                                            123 Main Street, City, Country
+                                        </p>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <br />
+                            <br />
+                            <Row>
+                                <Col md={12} className="footer-hr ">
+                                    <h5 className="footer-heading">Social Media</h5>
+                                    <ul className="social-media-icons">
+                                        {/* WhatsApp */}
+                                        <li><a href="#"><i className="fab fa-whatsapp fa-2x"></i></a></li>
+                                        {/* Instagram */}
+                                        <li><a href="#"><i className="fab fa-instagram fa-2x"></i></a></li>
+                                        {/* LinkedIn */}
+                                        <li><a href="#"><i className="fab fa-linkedin fa-2x"></i></a></li>
+                                        {/* Facebook */}
+                                        <li><a href="#"><i className="fab fa-facebook fa-2x"></i></a></li>
+                                        {/* Twitter */}
+                                        <li><a href="#"><i className="fab fa-twitter fa-2x"></i></a></li>
+                                    </ul>
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col md={3} className="footer-section">
-                            <h5 className="footer-heading">About</h5>
-                            {/* Add about content here */}
-                        </Col>
-                        <Col md={3} className="footer-section">
-                            <h5 className="footer-heading">Contact Us</h5>
-                            <ul className="footer-list">
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                                <li>Hello Name</li>
-                            </ul>
-                        </Col>
-                        <Col md={3} className="footer-section">
+                        <Col md={4} className="footer-section">
                             <h5 className="footer-heading">Our Sports</h5>
-                            <Carousel>
+                            <div className="row d-flex">
+                                {/* Map through imagePaths array and render each image */}
                                 {imagePaths.map((imagePath, index) => (
-                                    <Carousel.Item key={index}>
-                                        <img className="carousel-img" src={imagePath} alt={`Slide ${index + 1}`} />
-                                    </Carousel.Item>
+                                    <div key={index} className="col-md-1 footer-box">
+                                        <img src={imagePath} alt={`Image ${index + 1}`} style={{ width: "100%", height: '100%' }} />
+                                    </div>
                                 ))}
-                            </Carousel>
+                            </div>
                         </Col>
                     </Row>
                     <hr className="footer-divider" />
                     <Row>
-                        <Col md={6}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quos!</Col>
-                        <Col md={6} className="footer-hr">
-                            <h5 className="footer-heading">Social Media</h5>
-                            <ul className="social-media-icons">
-                                {/* WhatsApp */}
-                                <li><a href="#"><i className="fab fa-whatsapp"></i></a></li>
-                                {/* Instagram */}
-                                <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-                                {/* LinkedIn */}
-                                <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
-                                {/* Facebook */}
-                                <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-                                {/* Twitter */}
-                                <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                            </ul>
+                        <Col md={12}>
+                            <span>
+                                Privacy Policy | Terms & Conditions | Software License Agreement |
+                                Do Not Sell or Share My Personal Information | Cookies | Security
+                            </span>
+                            <br />
+                            <span>
+                                Novanj Analysis is a product and service of Novanj Sports Technologies, Inc.
+                                All text and design © 2007-2024. All rights reserved.
+                            </span>
                         </Col>
+
                     </Row>
                 </footer>
             </Container>
--- src/components/pages/subPages/Services.jsx
+++ src/components/pages/subPages/Services.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+import './Services.css'; // Import your CSS file for custom styles
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-function Services() {
+const Services = () => {
+  // Define the content array with objects representing each row
+  const content = [
+    {
+      imageUrl: process.env.PUBLIC_URL + './assets/images/bg-1.jpg', // Use local image path
+      heading: 'Hello World',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac elit id libero finibus consectetur. Quisque sollicitudin lorem eget ligula bibendum, id tempus dolor lacinia. Nulla facilisi. Integer pharetra urna nec elit placerat, et volutpat ligula consectetur. Sed varius, odio eget ultricies rutrum, sapien leo eleifend odio, eget laoreet lectus urna et est. Sed in felis sed ex commodo mattis.'
+    },
+    {
+      imageUrl: process.env.PUBLIC_URL + '/assets/images/logo.png',
+      heading: 'Hello Lalit',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac elit id libero finibus consectetur. Quisque sollicitudin lorem eget ligula bibendum, id tempus dolor lacinia. Nulla facilisi. Integer pharetra urna nec elit placerat, et volutpat ligula consectetur. Sed varius, odio eget ultricies rutrum, sapien leo eleifend odio, eget laoreet lectus urna et est. Sed in felis sed ex commodo mattis.'
+    },
+    {
+      imageUrl: process.env.PUBLIC_URL + '/assets/images/logo.png',
+      heading: 'Hello Sunny',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac elit id libero finibus consectetur. Quisque sollicitudin lorem eget ligula bibendum, id tempus dolor lacinia. Nulla facilisi. Integer pharetra urna nec elit placerat, et volutpat ligula consectetur. Sed varius, odio eget ultricies rutrum, sapien leo eleifend odio, eget laoreet lectus urna et est. Sed in felis sed ex commodo mattis.'
+    }
+  ];
+
   return (
-    <div>
-      <h1>services</h1>
-    </div>
-  )
+    <>
+      <Container>
+        <>
+          <Row className="justify-content-around mt-5">
+            <Col md={3} className="d-flex flex-column align-items-center p-5 custom-col">
+              <i className="fas fa-coffee fa-lg p-4"></i>
+              <h>hello</h>
+              <p>Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, adipisci?</p>
+            </Col>
+            <Col md={3} className="d-flex flex-column align-items-center p-5 custom-col">
+              <i className="fas fa-coffee fa-lg p-4"></i>
+              <h>hello</h>
+
+              <p>Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, adipisci?</p>
+            </Col>
+            <Col md={3} className="d-flex flex-column align-items-center p-5 custom-col">
+              <i className="fas fa-coffee fa-lg p-4"></i>
+              <h>hello</h>
+
+              <p>Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, adipisci?</p>
+            </Col>
+
+          </Row>
+          {/* Map over the content array to generate rows dynamically */}
+          {content.map((item, index) => (
+            <Row key={index} className="RowData">
+              {/* Alternate the image and text columns based on the row index */}
+              {(index % 2 === 0) ? (
+                <>
+                  {/* Image column */}
+                  <Col md={6} className="service-image">
+                    <div className="content-wrapper">
+                      <img src={item.imageUrl} alt="placeholder" />
+                    </div>
+                  </Col>
+                  {/* Text column */}
+                  <Col md={6} className="service-text">
+                    <div className="content-wrapper">
+                      <h4 className="textHead">{item.heading}</h4>
+                      <p className="textL">{item.text}</p>
+                    </div>
+                  </Col>
+                </>
+              ) : (
+                <>
+                  {/* Text column */}
+                  <Col md={6} className="service-text">
+                    <div className="content-wrapper">
+                      <h4 className="textHead">{item.heading}</h4>
+                      <p className="textL">{item.text}</p>
+                    </div>
+                  </Col>
+                  {/* Image column */}
+                  <Col md={6} className="service-image">
+                    <div className="content-wrapper">
+
+                      <img src={item.imageUrl} alt="placeholder" />
+                    </div>
+                  </Col>
+                </>
+              )}
+            </Row>
+          ))}
+        </>
+      </Container>
+    </>
+  );
 }
 
-export default Services
+export default Services;
